b177c90698d7e5a40d7cc485d8e56fea454c454e
Caio Ramos <caio.ramos@loft.com.br>
2021-11-19 22:01:07 -0300

    Primeira versão do jogo da forca finalizado
    Com regras para ganhar, perder e formatação do resultado.

--- jogo.py
+++ jogo.py
@@ -1,39 +1,41 @@
 # variavel com a palavra secreta
-palavra_secreta = "caioba"
+palavra_secreta = input("Digite a palavra secreta: ")
 
 # cria uma variavel de texto chamada resultado com "_" correspondentes a
 # quantidade de letras da palavra secreta
-resultado = ""
+resultado = []
 for posicao in range(1, len(palavra_secreta) + 1):
-    resultado += "_ "
+    resultado.append("_")
 
 # mostra os "_" correspondentes a quantidade de letras da palavra secreta
-print("Palavra secreta:", resultado)
+print("Palavra secreta:", " ".join(resultado))
 
 jogo_acabou = False
 chutes_certos = ""
+erros = 0
 
 # impede o programa de acabar
 while jogo_acabou == False:
     # cria uma variavel pra guardar a letra que a pessoa digitar
     letra_chute = input("Dá seu chute ai: ")
 
-    resultado = ""
-
     # verifica se a letra digitada pelo usuario está dentro da palavra secreta
     if letra_chute in palavra_secreta:
 
-        # armazena todos os chutes certos do usuário
-        chutes_certos += letra_chute
+        for posicao_da_letra in range(0, len(palavra_secreta)):
 
-        # para cada letra_da_palavra_secreta em/na palavra_secreta
-        for letra_da_palavra_secreta in palavra_secreta:
+            if palavra_secreta[posicao_da_letra] == letra_chute:
+                resultado[posicao_da_letra] = letra_chute
 
-            if letra_da_palavra_secreta == letra_chute:
-                resultado += letra_chute + " "
-            else:
-                resultado += "_ "
-
-        print(resultado)
+        print(" ".join(resultado).upper())
     else:
         print("Tente novamente! =(")
+        erros += 1
+
+    if "_" not in resultado:
+        print("Você ganhou!")
+        jogo_acabou = True
+
+    if erros == 3:
+        print("Cê si fudeu!")
+        jogo_acabou = True
